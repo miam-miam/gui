740d1b45948bb499ff971445df56d111fa51955c
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2023-11-27 12:17:21 +0000

    Update examples

--- examples/counter/main.rs
+++ examples/counter/main.rs
@@ -1,14 +1,22 @@
 #[derive(Component)]
 struct Counter {
-    count: Invadlidated<i32>,
-    disabled_decrement: Invalidated<bool>,
+    count: Invadlidated<u32>,
 }
+
+impl Invalidate for gen::Disabled<Counter> {
+    fn is_invalidated(&self)  {
+        count.is_invalidated()
+    }
+    fn value(&self) -> bool {
+        count.value() == 0
+    }
+}
+
 
 impl Default for Counter {
     fn default() -> Counter {
         Counter {
             count: 0.into(),
-            disabled_decrement: true.into(),
         }
     }
 }
@@ -16,18 +24,12 @@ impl Default for Counter {
 impl ButtonAction for gen::IncrementBtn<Counter> {
     fn on_press(&mut self, _e: MouseEvent) {
         self.count.as_mut() += 1;
-        if self.count == 1 {
-            self.disabled_decrement.as_mut() = false;
-        }
     }
 }
 
 impl ButtonAction for gen::DecrementBtn<Counter> {
     fn on_press(&mut self, _e: MouseEvent) {
         self.count.as_mut() -= 1;
-        if self.count == 0 {
-            self.disabled_decrement.as_mut() = true;
-        }
     }
 }
 
--- examples/todo/main.rs
+++ examples/todo/main.rs
@@ -1,62 +1,3 @@
-
-// Must be in separate modules due to #[derive(Component)] generating a mod gen.
-mod todos {
-    pub enum Message {
-        RemoveTask(usize)
-    }
-
-    impl OnMessage<Message> for Todos {
-        fn on_receive(&mut self, message: Message) {
-            match message {
-                Message::RemoveTask(id) => {
-                    self.tasks.as_mut().remove(id);
-                }
-            }
-        }
-    }
-
-    #[derive(Component, Default)]
-    pub struct Todos {
-        tasks: Invalidated<Vec<todo::Todo>>,
-        current_task_id: u32
-    }
-
-    impl InvalidateSumTasks for Todos {
-        fn invalidated(&self) -> bool {
-            self.tasks.invalidated()
-        }
-
-        fn value(&self) -> &u32 {
-            &self.tasks.value().iter().filter(|t| !t.completed).sum()
-        }
-    }
-
-    impl TextInputAction for gen::TodoDescription {
-        fn on_submit(&mut self, value: &str) {
-            tasks.as_mut().push(todo::Todo::new(value, tasks.as_ref().len()));
-        }
-    }
-
-    impl ButtonAction for gen::All {
-        fn on_press(&mut self, _e: MouseEvent) {
-            self.state.as_mut() = gen::states::All;
-        }
-    }
-
-    impl ButtonAction for gen::Active {
-        fn on_press(&mut self, _e: MouseEvent) {
-            self.state.as_mut() = gen::states::Active;
-        }
-    }
-
-    impl ButtonAction for gen::Completed {
-        fn on_press(&mut self, _e: MouseEvent) {
-            self.state.as_mut() = gen::states::Completed;
-        }
-    }
-}
-
-
 mod todo {
     #[derive(Component)]
     pub struct Todo {
@@ -68,7 +9,7 @@ mod todo {
     impl Todo {
         pub fn new(todo_name: &str, task_id: usize) -> Todo {
             Todo {
-                state: gen::states::Default(),
+                state: Invalidated::new(gen::state::Default()),
                 name: Invalidated::new(todo_name.into()),
                 completed: Invalidated::new(false),
                 id: task_id,
@@ -84,24 +25,79 @@ mod todo {
 
     impl ButtonAction for gen::EditBtn {
         fn on_press(&mut self, _e: MouseEvent) {
-            self.state.as_mut() = gen::states::Edit;
+            self.state.as_mut() = gen::state::Edit;
         }
     }
 
     impl ButtonAction for gen::DeleteBtn {
         fn on_press(&mut self, e: MouseEvent) {
-            e.ctx().send_message(todo::Message::RemoveTask(self.id));
+            e.ctx().send_message(super::todos::Message::RemoveTask(self.id));
         }
     }
 
     impl TextInputAction for gen::NameInput {
         fn on_submit(&mut self, value: &str) {
             self.name.as_mut() = value.into();
-            self.state.as_mut() = gen::states::View;
+            self.state.as_mut() = gen::state::View;
         }
     }
 }
 
+// Must be in separate modules due to #[derive(Component)] generating a mod gen.
+mod todos {
+    #[derive(Component, Default)]
+    pub struct Todos {
+        tasks: Invalidated<Vec<super::todo::Todo>>,
+    }
+
+    pub enum Message {
+        RemoveTask(usize)
+    }
+
+    impl OnMessage<Message> for Todos {
+        fn on_receive(&mut self, message: Message) {
+            match message {
+                Message::RemoveTask(id) => {
+                    self.tasks.as_mut().remove(id);
+                }
+            }
+        }
+    }
+
+    impl Invalidate for gen::SumTasks<Todos> {
+        fn invalidated(&self) -> bool {
+            self.tasks.invalidated()
+        }
+
+        fn value(&self) -> u32 {
+            &self.tasks.value().iter().filter(|t| !t.completed).sum()
+        }
+    }
+
+    impl TextInputAction for gen::TodoDescription {
+        fn on_submit(&mut self, value: &str) {
+            self.tasks.as_mut().push(super::todo::Todo::new(value, tasks.as_ref().len()));
+        }
+    }
+
+    impl ButtonAction for gen::All {
+        fn on_press(&mut self, _e: MouseEvent) {
+            self.state.as_mut() = gen::state::All;
+        }
+    }
+
+    impl ButtonAction for gen::Active {
+        fn on_press(&mut self, _e: MouseEvent) {
+            self.state.as_mut() = gen::state::Active;
+        }
+    }
+
+    impl ButtonAction for gen::Completed {
+        fn on_press(&mut self, _e: MouseEvent) {
+            self.state.as_mut() = gen::state::Completed;
+        }
+    }
+}
 fn main() {
     gui::run("Todo App", todos::Todos::default())
 }
