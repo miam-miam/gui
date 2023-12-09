c95a67c1ca663f7685b2018bbcd6054028972fbe
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2023-12-09 17:41:06 +0000

    Add simple project layout

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 ## Why gui?
 
 To prevent bike shedding, I'll come up with a better name when the project needs one.
+
+## Dependencies
+
+Gui requires a recent rust toolchain to build; it does not (yet) have an
+explicit minimum supported rust version, but the latest stable version should
+work.
+
+On Linux and BSD, Gui also requires `pkg-config` and `clang`,
+and the development packages of `wayland`, `libxkbcommon` and `libxcb`, to be installed.
+Some of the examples require `vulkan-loader`.
+
+Most distributions have `pkg-config` installed by default. To install the remaining packages on Fedora, run
+```sh
+sudo dnf install clang wayland-devel libxkbcommon-x11-devel libxcb-devel vulkan-loader-devel
+```
+To install them on Debian or Ubuntu, run
+```sh
+sudo apt-get install pkg-config clang libwayland-dev libxkbcommon-x11-dev libvulkan-dev
+```
+
---
+++ examples/shello/src/main.rs
@@ -0,0 +1,323 @@
+use std::any::Any;
+use tracing_subscriber::EnvFilter;
+
+mod common {
+    pub mod text {
+        use gui::parley::Layout;
+        use gui::vello::kurbo::Affine;
+        use gui::vello::{
+            glyph::{fello::raw::FontRef, GlyphContext},
+            peniko::{Brush, Color},
+            *,
+        };
+
+        #[derive(Clone, PartialEq, Debug)]
+        pub struct ParleyBrush(pub Brush);
+
+        impl Default for ParleyBrush {
+            fn default() -> ParleyBrush {
+                ParleyBrush(Brush::Solid(Color::rgb8(0, 0, 0)))
+            }
+        }
+
+        impl gui::parley::style::Brush for ParleyBrush {}
+
+        pub fn render_text(
+            builder: &mut SceneBuilder,
+            transform: Affine,
+            layout: &Layout<ParleyBrush>,
+        ) {
+            let mut gcx = GlyphContext::new();
+            for line in layout.lines() {
+                for glyph_run in line.glyph_runs() {
+                    let mut x = glyph_run.offset();
+                    let y = glyph_run.baseline();
+                    let run = glyph_run.run();
+                    let font = run.font();
+                    let font_size = run.font_size();
+                    let font_ref = font.as_ref();
+                    if let Ok(font_ref) = FontRef::from_index(font_ref.data, font.index()) {
+                        let style = glyph_run.style();
+                        let vars: [(&str, f32); 0] = [];
+                        let mut gp = gcx.new_provider(&font_ref, None, font_size, false, vars);
+                        for glyph in glyph_run.glyphs() {
+                            if let Some(fragment) = gp.get(glyph.id, Some(&style.brush.0)) {
+                                let gx = x + glyph.x;
+                                let gy = y - glyph.y;
+                                let xform = Affine::translate((gx as f64, gy as f64))
+                                    * Affine::scale_non_uniform(1.0, -1.0);
+                                builder.append(&fragment, Some(transform * xform));
+                            }
+                            x += glyph.advance;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[cfg(feature = "accesskit")]
+use accesskit::TreeUpdate;
+use gui::parley::FontContext;
+use gui::vello::util::{RenderContext, RenderSurface};
+use gui::vello::Renderer;
+use gui::vello::{
+    kurbo::{Affine, PathEl, Point, Rect},
+    peniko::{Brush, Color, Fill, Mix, Stroke},
+    RenderParams, RendererOptions, Scene, SceneBuilder,
+};
+
+use crate::common::text::ParleyBrush;
+use gui::glazier::kurbo::Size;
+use gui::glazier::{
+    Application, Cursor, FileDialogToken, FileInfo, IdleToken, KeyEvent, PointerEvent, Region,
+    Scalable, TimerToken, WinHandler, WindowHandle,
+};
+
+const WIDTH: usize = 2048;
+const HEIGHT: usize = 1536;
+
+fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+    let app = Application::new().unwrap();
+    let window = gui::glazier::WindowBuilder::new(app.clone())
+        .size((WIDTH as f64 / 2., HEIGHT as f64 / 2.).into())
+        .handler(Box::new(WindowState::new()))
+        .build()
+        .unwrap();
+    window.show();
+    app.run(None);
+}
+//
+struct WindowState {
+    handle: WindowHandle,
+    renderer: Option<Renderer>,
+    render: RenderContext,
+    surface: Option<RenderSurface>,
+    scene: Scene,
+    size: Size,
+    font_context: FontContext,
+    counter: u64,
+}
+
+impl WindowState {
+    pub fn new() -> Self {
+        let render = RenderContext::new().unwrap();
+        Self {
+            handle: Default::default(),
+            surface: None,
+            renderer: None,
+            render,
+            scene: Default::default(),
+            font_context: FontContext::new(),
+            counter: 0,
+            size: Size::new(800.0, 600.0),
+        }
+    }
+
+    fn schedule_render(&self) {
+        self.handle.invalidate();
+    }
+
+    fn surface_size(&self) -> (u32, u32) {
+        let handle = &self.handle;
+        let scale = handle.get_scale().unwrap_or_default();
+        let insets = handle.content_insets().to_px(scale);
+        let mut size = handle.get_size().to_px(scale);
+        size.width -= insets.x_value();
+        size.height -= insets.y_value();
+        (size.width as u32, size.height as u32)
+    }
+
+    fn render(&mut self) {
+        let (width, height) = self.surface_size();
+        if self.surface.is_none() {
+            self.surface = Some(
+                pollster::block_on(self.render.create_surface(&self.handle, width, height))
+                    .expect("failed to create surface"),
+            );
+        }
+
+        render_anim_frame(&mut self.scene, &mut self.font_context, self.counter);
+        self.counter += 1;
+
+        if let Some(surface) = self.surface.as_mut() {
+            if surface.config.width != width || surface.config.height != height {
+                self.render.resize_surface(surface, width, height);
+            }
+            let surface_texture = surface.surface.get_current_texture().unwrap();
+            let dev_id = surface.dev_id;
+            let device = &self.render.devices[dev_id].device;
+            let queue = &self.render.devices[dev_id].queue;
+            let renderer_options = RendererOptions {
+                surface_format: Some(surface.format),
+                timestamp_period: queue.get_timestamp_period(),
+            };
+            let render_params = RenderParams {
+                base_color: Color::BLACK,
+                width,
+                height,
+            };
+            self.renderer
+                .get_or_insert_with(|| Renderer::new(device, &renderer_options).unwrap())
+                .render_to_surface(device, queue, &self.scene, &surface_texture, &render_params)
+                .unwrap();
+            surface_texture.present();
+        }
+    }
+}
+
+impl WinHandler for WindowState {
+    fn connect(&mut self, handle: &WindowHandle) {
+        self.handle = handle.clone();
+        self.schedule_render();
+    }
+
+    fn prepare_paint(&mut self) {}
+
+    fn paint(&mut self, _: &Region) {
+        self.render();
+        self.schedule_render();
+    }
+
+    fn idle(&mut self, _: IdleToken) {}
+
+    fn command(&mut self, _id: u32) {}
+
+    fn open_file(&mut self, _token: FileDialogToken, file_info: Option<FileInfo>) {
+        println!("open file result: {file_info:?}");
+    }
+
+    fn save_as(&mut self, _token: FileDialogToken, file: Option<FileInfo>) {
+        println!("save file result: {file:?}");
+    }
+
+    fn key_down(&mut self, event: &KeyEvent) -> bool {
+        println!("keydown: {event:?}");
+        false
+    }
+
+    fn key_up(&mut self, event: &KeyEvent) {
+        println!("keyup: {event:?}");
+    }
+
+    fn wheel(&mut self, event: &PointerEvent) {
+        println!("wheel {event:?}");
+    }
+
+    fn pointer_move(&mut self, _event: &PointerEvent) {
+        self.handle.set_cursor(&Cursor::Arrow);
+        //println!("pointer_move {event:?}");
+    }
+
+    fn pointer_down(&mut self, event: &PointerEvent) {
+        println!("pointer_down {event:?}");
+    }
+
+    fn pointer_up(&mut self, event: &PointerEvent) {
+        println!("pointer_up {event:?}");
+    }
+
+    fn timer(&mut self, id: TimerToken) {
+        println!("timer fired: {id:?}");
+    }
+
+    fn size(&mut self, size: Size) {
+        self.size = size;
+    }
+
+    fn got_focus(&mut self) {
+        println!("Got focus");
+    }
+
+    fn lost_focus(&mut self) {
+        println!("Lost focus");
+    }
+
+    fn request_close(&mut self) {
+        self.handle.close();
+    }
+
+    fn destroy(&mut self) {
+        Application::global().quit();
+    }
+
+    fn as_any(&mut self) -> &mut dyn Any {
+        self
+    }
+}
+
+pub fn render_anim_frame(scene: &mut Scene, fcx: &mut FontContext, i: u64) {
+    let mut sb = SceneBuilder::for_scene(scene);
+    let rect = Rect::from_origin_size(Point::new(0.0, 0.0), (1000.0, 1000.0));
+    sb.fill(
+        Fill::NonZero,
+        Affine::IDENTITY,
+        &Brush::Solid(Color::rgb8(128, 128, 128)),
+        None,
+        &rect,
+    );
+
+    let scale = (i as f64 * 0.01).sin() * 0.5 + 1.5;
+    let mut lcx = gui::parley::LayoutContext::new();
+    let mut layout_builder =
+        lcx.ranged_builder(fcx, "Hello vello! ഹലോ ਸਤ ਸ੍ਰੀ ਅਕਾਲ مرحبا!", scale as f32);
+    layout_builder.push_default(&gui::parley::style::StyleProperty::FontSize(34.0));
+    layout_builder.push(
+        &gui::parley::style::StyleProperty::Brush(ParleyBrush(Brush::Solid(Color::rgb8(
+            255, 255, 0,
+        )))),
+        6..10,
+    );
+    layout_builder.push(&gui::parley::style::StyleProperty::FontSize(48.0), 6..12);
+    layout_builder.push(
+        &gui::parley::style::StyleProperty::Brush(ParleyBrush(Color::YELLOW.into())),
+        6..12,
+    );
+    layout_builder.push_default(&gui::parley::style::StyleProperty::Brush(ParleyBrush(
+        Brush::Solid(Color::rgb8(255, 255, 255)),
+    )));
+    let mut layout = layout_builder.build();
+    layout.break_all_lines(None, gui::parley::layout::Alignment::Start);
+    common::text::render_text(&mut sb, Affine::translate((100.0, 400.0)), &layout);
+
+    let th = (std::f64::consts::PI / 180.0) * (i as f64);
+    let center = Point::new(500.0, 500.0);
+    let mut p1 = center;
+    p1.x += 400.0 * th.cos();
+    p1.y += 400.0 * th.sin();
+    sb.stroke(
+        &Stroke::new(5.0),
+        Affine::IDENTITY,
+        &Brush::Solid(Color::rgb8(128, 0, 0)),
+        None,
+        &[PathEl::MoveTo(center), PathEl::LineTo(p1)],
+    );
+    sb.fill(
+        Fill::NonZero,
+        Affine::translate((150.0, 150.0)) * Affine::scale(0.2),
+        Color::RED,
+        None,
+        &rect,
+    );
+    let alpha = (i as f64 * 0.03).sin() as f32 * 0.5 + 0.5;
+    sb.push_layer(Mix::Normal, alpha, Affine::IDENTITY, &rect);
+    sb.fill(
+        Fill::NonZero,
+        Affine::translate((100.0, 100.0)) * Affine::scale(0.2),
+        Color::BLUE,
+        None,
+        &rect,
+    );
+    sb.fill(
+        Fill::NonZero,
+        Affine::translate((200.0, 200.0)) * Affine::scale(0.2),
+        Color::GREEN,
+        None,
+        &rect,
+    );
+    sb.pop_layer();
+}
